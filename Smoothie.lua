1fa2116aa568511b793e7c6f66843513cdcbe8e9
Sh1zok <97683139+Sh1zok@users.noreply.github.com>
2025-09-11 21:11:23 +0300

    Ears released

--- Smoothie.lua
+++ Smoothie.lua
@@ -1,7 +1,7 @@
 --[[
     ■■■■■ Smoothie
     ■   ■ Author: Sh1zok
-    ■■■■  v0.60
+    ■■■■  v0.7.0
 
 MIT License
 
@@ -32,7 +32,7 @@ local smoothie = {}
 
 function smoothie:newSmoothHead(modelPart)
     -- Checking the validity of the parameter
-    assert(type(modelPart) == "ModelPart", "Invalid argument to function newSmoothHead. Expected ModelPart, but got " .. type(table))
+    assert(type(modelPart) == "ModelPart", "Invalid argument to function newSmoothHead. Expected ModelPart, but got " .. type(modelPart))
     modelPart:setParentType("NONE") -- Preparing modelPart
 
     -- Setting up some variables
@@ -117,7 +117,7 @@ end
 
 function smoothie:newEye(modelPart)
     -- Checking the validity of the parameter
-    assert(type(modelPart) == "ModelPart", "Invalid argument to function newEye. Expected ModelPart, but got " .. type(table))
+    assert(type(modelPart) == "ModelPart", "Invalid argument to function newEye. Expected ModelPart, but got " .. type(modelPart))
 
     -- Setting up some variables
     local interface = {}
@@ -156,7 +156,7 @@ function smoothie:newEye(modelPart)
 
         return interface
     end
-    function interface:topOffsetStrenght(value) return interface:setTopOffsetStrenght(value) end
+    function interface:topOffsetStrenght(value) return interface:setTopOffsetStrenght(value) end -- Alias
 
     function interface:setBottomOffsetStrenght(value)
         assert(type(value) == "number", "Invalid argument to function setBottomOffsetStrenght. Expected number, but got " .. type(value))
@@ -164,7 +164,7 @@ function smoothie:newEye(modelPart)
 
         return interface
     end
-    function interface:bottomOffsetStrenght(value) return interface:setBottomOffsetStrenght(value) end
+    function interface:bottomOffsetStrenght(value) return interface:setBottomOffsetStrenght(value) end -- Alias
 
     function interface:setLeftOffsetStrenght(value)
         assert(type(value) == "number", "Invalid argument to function setLeftOffsetStrenght. Expected number, but got " .. type(value))
@@ -172,7 +172,7 @@ function smoothie:newEye(modelPart)
 
         return interface
     end
-    function interface:leftOffsetStrenght(value) return interface:setLeftOffsetStrenght(value) end
+    function interface:leftOffsetStrenght(value) return interface:setLeftOffsetStrenght(value) end -- Alias
 
     function interface:setRightOffsetStrenght(value)
         assert(type(value) == "number", "Invalid argument to function setRightOffsetStrenght. Expected number, but got " .. type(value))
@@ -180,7 +180,89 @@ function smoothie:newEye(modelPart)
 
         return interface
     end
-    function interface:rightOffsetStrenght(value) return interface:setRightOffsetStrenght(value) end
+    function interface:rightOffsetStrenght(value) return interface:setRightOffsetStrenght(value) end -- Alias
+
+    return interface
+end
+
+
+
+function smoothie:newEar(modelPart)
+    -- Checking the validity of the parameter
+    assert(type(modelPart) == "ModelPart", "Invalid argument to function newHorizontalEar. Expected ModelPart, but got " .. type(modelPart))
+
+    -- Setting up some variables
+    local interface = {}
+    local earModelPart = modelPart
+    local headRotationDelta, prevHeadRotationAngle = vec(0, 0, 0), vec(0, 0, 0)
+    local speed, bouncy = 1, 1
+    local earRotationVelocity = vec(0, 0, 0)
+    local rotationLimits = {top = 10, bottom = 10, left = 45, right = 45}
+
+    -- Ear rotation logic
+    events.RENDER:register(function()
+        -- Calculating the difference in head rotation
+        local headRotationAngle = (vanilla_model.HEAD:getOriginRot() + 180) % 360 - 180
+        headRotationDelta = headRotationDelta + (prevHeadRotationAngle - headRotationAngle)
+        headRotationDelta[1] = math.clamp(headRotationDelta[1], -rotationLimits.bottom, rotationLimits.top)
+        headRotationDelta[2] = math.clamp(headRotationDelta[2], -rotationLimits.left, rotationLimits.right)
+        prevHeadRotationAngle = headRotationAngle
+
+        -- Calculation the speed of rotation and the rotation of the ear itself
+        earRotationVelocity = earRotationVelocity + -(speed * (earModelPart:getOffsetRot() - headRotationDelta) + math.sqrt(speed * 20) * bouncy * earRotationVelocity) / client:getFPS()
+        earModelPart:setOffsetRot(earModelPart:getOffsetRot() + earRotationVelocity / client:getFPS())
+
+        -- Soft reduction of head deviation to zeros
+        headRotationDelta = math.lerp(headRotationDelta, vec(0, 0, 0), 5 / client:getFPS())
+    end, "Smoothie.horizontalEarProcessor")
+
+    function interface:setBouncy(value)
+        assert(type(value) == "number", "Invalid argument to function setBouncy. Expected number, but got " .. type(value))
+        bouncy = value
+
+        return interface
+    end
+    function interface:boucny(value) return interface:setBouncy(value) end -- Alias
+
+    function interface:setSpeed(value)
+        assert(type(value) == "number", "Invalid argument to function setSpeed. Expected number, but got " .. type(value))
+        speed = value
+
+        return interface
+    end
+    function interface:speed(value) return interface:setSpeed(value) end -- Alias
+
+    function interface:setTopLimit(value)
+        assert(type(value) == "number", "Invalid argument to function setTopLimit. Expected number, but got " .. type(value))
+        rotationLimits.top = value
+
+        return interface
+    end
+    function interface:topLimit(value) return interface:setTopLimit(value) end -- Alias
+
+    function interface:setBottomLimit(value)
+        assert(type(value) == "number", "Invalid argument to function setBottomLimit. Expected number, but got " .. type(value))
+        rotationLimits.bottom = value
+
+        return interface
+    end
+    function interface:bottomLimit(value) return interface:setBottomLimit(value) end -- Alias
+
+    function interface:setLeftLimit(value)
+        assert(type(value) == "number", "Invalid argument to function setLeftLimit. Expected number, but got " .. type(value))
+        rotationLimits.left = value
+
+        return interface
+    end
+    function interface:leftLimit(value) return interface:setLeftLimit(value) end -- Alias
+
+    function interface:setRightLimit(value)
+        assert(type(value) == "number", "Invalid argument to function setRightLimit. Expected number, but got " .. type(value))
+        rotationLimits.right = value
+
+        return interface
+    end
+    function interface:rightLimit(value) return interface:setRightLimit(value) end -- Alias
 
     return interface
 end
